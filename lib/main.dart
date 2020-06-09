815a3c532ec24c43227a9a6164e3e1e4af61d44c
Jonathan Kutsch <66543465+JonathanKutsch@users.noreply.github.com>
2020-06-08 22:34:50 -0500

    Upload lib folder

---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:my_block/model/database.dart';
+import 'package:my_block/pages/add_event_page.dart';
+import 'package:my_block/pages/add_task_page.dart';
+import 'package:my_block/pages/event_page.dart';
+import 'package:my_block/pages/task_page.dart';
+import 'package:my_block/widgets/custom_button.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider<Database> (builder: (_) => Database())],
+      child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Block',
+      theme: ThemeData(primarySwatch: Colors.indigo, fontFamily: "Roboto"),
+      home: MyHomePage(),
+    ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  PageController _pageController = PageController();
+
+  double currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    _pageController.addListener(() {
+      setState(() {
+        currentPage = _pageController.page;
+      });
+    });
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: 35,
+            color: Theme.of(context).accentColor,
+          ),
+          Positioned(
+            right: 0,
+            child: Text(
+              "17",
+              style: TextStyle(fontSize: 200, color: Color(0x10000000)),
+            ),
+          ),
+          _mainContent(context),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                child: currentPage == 0 ? AddTaskPage() : AddEventPage(),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12))));
+            });
+      },
+      child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods
+    );
+  }
+
+  Widget _mainContent(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 60),
+          Padding(
+            padding:const EdgeInsets.all(24.0),
+            child: Text(
+              "Tuesday",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ), // Text
+          ), // Padding
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: _button(context),
+          ),
+          Expanded(
+            child: PageView(
+            controller: _pageController,
+            children: <Widget>[TaskPage(), EventPage()],
+          ))
+      ],
+    );
+  }
+
+  Widget _button(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: CustomButton(
+            onPressed: () {
+              _pageController.previousPage(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.bounceInOut);
+            },
+            buttonText: "Tasks",
+            color: 
+                currentPage < 0.5 ? Theme.of(context).accentColor : Colors.white,
+            textColor: 
+                currentPage < 0.5 ? Colors.white : Theme.of(context).accentColor,
+            borderColor: currentPage < 0.5
+            ? Colors.transparent
+            : Theme.of(context).accentColor,
+          )),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+          child: CustomButton(
+            onPressed: () {
+              _pageController.nextPage(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.bounceInOut);
+            },
+            buttonText: "Events",
+            color: 
+            currentPage > 0.5 ? Theme.of(context).accentColor : Colors.white,
+            textColor:
+            currentPage > 0.5 ? Colors.white : Theme.of(context).accentColor,
+            borderColor: currentPage > 0.5
+            ? Colors.transparent
+            : Theme.of(context).accentColor,
+          ))
+      ],
+    );
+  }
+}
